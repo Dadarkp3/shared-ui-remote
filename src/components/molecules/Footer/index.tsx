a2e93be970f9cd350f18d2cd75e6fb6c9f9fa8fc
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2025-05-29 15:50:46 -0300

    feat: change footer

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="w-full p-4 bg-gray-100 text-center text-sm text-gray-600">
-    © 2025 Your Company. All rights reserved.
+    © 2025 Dadarkp3. All rights reserved.
   </footer>
 );
 
