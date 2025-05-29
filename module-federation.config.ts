46d284957d579eebfb69cc1fea9851e1979b7ff7
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2025-05-28 22:05:58 -0300

    remove counter

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -2,7 +2,6 @@ export const mfConfig = {
   name: "shared_ui_remote",
   filename: "remoteEntry.js",
   exposes: {
-    "./Counter": "./src/components/Counter",
     "./Navbar": "./src/components/organisms/Navbar",
   },
   shared: ["react", "react-dom", "react-router-dom"],
